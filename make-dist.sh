4741d4ada421fd16b86853756775f2f1eb24c206
megath <megath@bed45a8c-4017-4030-b681-87f170cefae6>
2007-04-02 07:21:57 +0000

    removed .. from path's

--- make-dist.sh
+++ make-dist.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-VER=`svnversion -cn .`
+VER=`svnversion -cn trunk`
 REV=`echo "$VER" | cut -d: -f 2`
 DIR=btanks-0.4.$REV
 echo "revision $REV"
 echo "cleaning up..."
 rm -rf ../$DIR
 echo "exporting..."
-svn export . ../$DIR
-echo -n $VER > ../$DIR/.svnversion
-rm -f ../$DIR/make-dist.sh
+svn export trunk $DIR
+echo -n $VER > $DIR/.svnversion
+rm -f $DIR/make-dist.sh
