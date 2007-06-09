cc867dbcc86f6736b543cd938d666752fbc62e80
megath <megath@bed45a8c-4017-4030-b681-87f170cefae6>
2007-06-09 07:29:35 +0000

    remove test map from release

--- make-dist.sh
+++ make-dist.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 VER=`svnversion -cn trunk`
 REV=`echo "$VER" | cut -d: -f 2`
-DIR=btanks-0.4.$REV
+DIR=btanks-0.5.$REV
 echo "revision $REV"
 echo "cleaning up..."
 rm -rf ../$DIR
 echo "exporting..."
 svn export trunk $DIR
 echo -n $VER > $DIR/.svnversion
-rm -f $DIR/make-dist.sh
+rm -f $DIR/data/maps/test*
