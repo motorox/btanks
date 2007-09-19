6ae652730881a5316de82f27dc5911c354f1e5ec
megath <megath@bed45a8c-4017-4030-b681-87f170cefae6>
2007-09-19 08:11:08 +0000

    updated version

--- make-dist.sh
+++ make-dist.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 VER=`svnversion -cn trunk`
 REV=`echo "$VER" | cut -d: -f 2`
-DIR=btanks-0.5.$REV
+VER_LONG="0.6.$REV"
+DIR="btanks-$VER_LONG"
 echo "revision $REV"
 echo "cleaning up..."
 rm -rf ../$DIR
@@ -9,3 +10,6 @@ echo "exporting..."
 svn export trunk $DIR
 echo -n $VER > $DIR/.svnversion
 rm -f $DIR/data/maps/test*
+echo "packing..."
+tar cjf btanks-$VER_LONG.tar.bz2 btanks-$VER_LONG
+echo "done..."
