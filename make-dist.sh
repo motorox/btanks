04f5a1ff25db683e603163ec2b235e28da23c55f
megath <megath@bed45a8c-4017-4030-b681-87f170cefae6>
2008-06-16 11:13:48 +0000

    pumped version to 0.8

--- make-dist.sh
+++ make-dist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 VER=`svnversion -cn trunk`
 REV=`echo "$VER" | cut -d: -f 2`
-VER_LONG="0.7.$REV"
+VER_LONG="0.8.$REV"
 DIR="btanks-$VER_LONG"
 echo "revision $REV"
 echo "cleaning up..."
