cb7fa164afc06736c03e2a1621abbb3e2e3d1701
megath <megath@bed45a8c-4017-4030-b681-87f170cefae6>
2008-11-13 18:59:38 +0000

    saved 32bit backup of original tiles

--- engine/src/registrar.cpp
+++
@@ -1,6 +0,0 @@
-#include "registrar.h"
-#include "resource_manager.h"
-
-void Registrar::registerObject(const std::string &name, Object *object) {
-	ResourceManager->registerObject(name, object);
-}
